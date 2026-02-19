04156c4687a1957db0f5a5d03a4edc488bea30b4
itupitsyn <itupitsyn@ptsecurity.com>
2026-02-19 22:53:49 +0300

    posters

--- web/lib/components/posters.tsx
+++ web/lib/components/posters.tsx
@@ -6,17 +6,24 @@ import Image from 'next/image';
 import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import p1 from '../../lib/assets/posters/p1.jpg';
-import p2 from '../../lib/assets/posters/p2.jpg';
-import p3 from '../../lib/assets/posters/p3.jpg';
-import p4 from '../../lib/assets/posters/p4.jpg';
-import p5 from '../../lib/assets/posters/p5.jpg';
-import p6 from '../../lib/assets/posters/p6.jpg';
-import p7 from '../../lib/assets/posters/p7.jpg';
-import p8 from '../../lib/assets/posters/p8.jpg';
+import p1 from '../../lib/assets/posters/p1.webp';
+import p2 from '../../lib/assets/posters/p2.webp';
+import p3 from '../../lib/assets/posters/p3.webp';
+import p4 from '../../lib/assets/posters/p4.webp';
+import p5 from '../../lib/assets/posters/p5.webp';
+import p6 from '../../lib/assets/posters/p6.webp';
+import p7 from '../../lib/assets/posters/p7.webp';
+import p8 from '../../lib/assets/posters/p8.webp';
+import p9 from '../../lib/assets/posters/p9.webp';
+import p10 from '../../lib/assets/posters/p10.webp';
+import p11 from '../../lib/assets/posters/p11.webp';
+import p12 from '../../lib/assets/posters/p12.webp';
+import p13 from '../../lib/assets/posters/p13.webp';
+import p14 from '../../lib/assets/posters/p14.webp';
+import p15 from '../../lib/assets/posters/p15.webp';
 import { cn } from '../utils/cn';
 
-const images = [p1, p2, p3, p4, p5, p6, p7, p8];
+const images = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15];
 
 export const Posters: FC = () => {
   return (
